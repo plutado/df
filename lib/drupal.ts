03e2ddc8455f6587504b449df41df1e4db7f6fc6
David Plutado Fugate <david@davidfugate.com>
2022-08-08 23:49:08 -0500

    use subdomain for content

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -1,8 +1,11 @@
-import { DrupalClient } from "next-drupal"
+import { DrupalClient } from 'next-drupal';
 
 export const drupal = new DrupalClient(
   process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
   {
-    previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
+    auth: {
+      username: process.env.DRUPAL_USERNAME,
+      password: process.env.DRUPAL_PASSWORD,
+    },
   }
-)
+);
